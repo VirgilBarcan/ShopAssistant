f753f461ce0db08c5ee5e26676938df50128146c
Virgil Barcan <ghitza9404@gmail.com>
2015-12-09 00:57:49 +0200

    New implementation of NavigationDrawer, with possibility to select an action; Some code was kept despite being useless;

---
+++ app/src/main/java/barcan/virgil/com/shopassistant/frontend/UserLoggedHomeFragment.java
@@ -0,0 +1,22 @@
+package barcan.virgil.com.shopassistant.frontend;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import barcan.virgil.com.shopassistant.R;
+
+/**
+ * Created by virgil on 08.12.2015.
+ */
+public class UserLoggedHomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.user_logged_home_fragment, container, false);
+        return view;
+    }
+
+}
--- app/src/main/java/barcan/virgil/com/shopassistant/frontend/UserMainScreenActivity.java
+++ app/src/main/java/barcan/virgil/com/shopassistant/frontend/UserMainScreenActivity.java
@@ -2,29 +2,30 @@ package barcan.virgil.com.shopassistant.frontend;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import barcan.virgil.com.shopassistant.R;
 import barcan.virgil.com.shopassistant.backend.Controller;
-import barcan.virgil.com.shopassistant.backend.RecyclerViewAdapter;
+import barcan.virgil.com.shopassistant.model.User;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserMainScreenActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter recyclerViewAdapter;
-    private RecyclerView.LayoutManager recyclerViewLayoutManager;
+    private NavigationView navigationView;
     private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     private Controller controller;
 
@@ -38,33 +39,80 @@ public class UserMainScreenActivity extends AppCompatActivity {
 
         controller = Controller.getInstance();
 
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+        addDataToNavigationViewHeader(navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                }
+                else {
+                    menuItem.setChecked(true);
+                }
 
-        recyclerViewAdapter = new RecyclerViewAdapter(controller.getConnectedUser());
-        recyclerView.setAdapter(recyclerViewAdapter);
+                drawerLayout.closeDrawers();
 
-        recyclerViewLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+                System.out.println("UserMainScreenActivity.onNavigationItemSelected: menuItem.getItemId()=" + menuItem.getItemId());
+
+                switch (menuItem.getItemId()) {
+                    case R.id.goToHome:
+                        Toast.makeText(getApplicationContext(), "Home selected", Toast.LENGTH_SHORT).show();
+
+                        UserLoggedHomeFragment fragment = new UserLoggedHomeFragment();
+                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.frame, fragment);
+                        fragmentTransaction.commit();
+                        return true;
+
+                    case R.id.goToProducts:
+                        Toast.makeText(getApplicationContext(), "Search products selected", Toast.LENGTH_SHORT).show();
+
+                        //TODO: Create fragments and classes
+
+                        return true;
+
+                    case R.id.shoppingList:
+                        Toast.makeText(getApplicationContext(), "Shopping list selected", Toast.LENGTH_SHORT).show();
+
+                        //TODO: Create fragments and classes
+
+                        return true;
+
+                    case R.id.goToAccount:
+                        Toast.makeText(getApplicationContext(), "Account selected", Toast.LENGTH_SHORT).show();
+
+                        //TODO: Create fragments and classes
+
+                        return true;
+
+                    default:
+                        System.out.println("UserMainScreenActivity.onNavigationItemSelected: ERROR! Id not supported!");
+                }
+                return false;
+            }
+        });
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             @Override
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                // code here will execute once the drawer is opened( As I dont want anything happened whe drawer is
-                // open I am not going to put anything here)
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes.
+                // As we don't want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
             }
 
             @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-                // Code here will execute once drawer is closed
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer opens.
+                // As we don't want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
             }
         };
 
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
         actionBarDrawerToggle.syncState();
     }
 
@@ -91,7 +139,7 @@ public class UserMainScreenActivity extends AppCompatActivity {
                 //TODO: React to search
                 break;
             case R.id.action_shopping_list:
-                System.out.println("MainActivity.onOptionsItemSelected: ShoppingList pressed!");
+                System.out.println("MainActivity.onOptionsItemSelected: Shopping List pressed!");
                 //TODO: React to shopping list
                 openShoppingListActivity();
                 break;
@@ -116,14 +164,29 @@ public class UserMainScreenActivity extends AppCompatActivity {
         startActivity(intentShoppingListActivity);
     }
 
-    /**
-     * This method is called when the user presses the Account button
-     * It opens the UserAccountActivity and shows the user's shopping list
-     */
-    private void openUserAccountActivity() {
-        Intent intentShoppingListActivity = new Intent(UserMainScreenActivity.this, UserShoppingListActivity.class);
-        //intentShoppingListActivity.putExtra("KEY", "value");
-        startActivity(intentShoppingListActivity);
+    private void addDataToNavigationViewHeader(NavigationView navigationView) {
+        User connectedUser = controller.getConnectedUser();
+
+        View headerView = LayoutInflater.from(this).inflate(R.layout.navigation_drawer_header, null);
+
+        System.out.println("UserMainScreenActivity.addDataToNavigationViewHeader: connectedUser=" + connectedUser);
+
+        if (connectedUser != null) {
+            TextView textViewFullName = (TextView) headerView.findViewById(R.id.fullname);
+            textViewFullName.setText(connectedUser.getFullName());
+
+            TextView textViewUsername = (TextView) headerView.findViewById(R.id.username);
+            textViewUsername.setText(connectedUser.getUsername());
+
+            //TODO: Add user image to the header
+            CircleImageView circleImageView = (CircleImageView) headerView.findViewById(R.id.circleView);
+            circleImageView.setImageResource(R.drawable.ic_account_circle);
+
+            navigationView.addHeaderView(headerView);
+        }
+        else {
+            System.out.println("UserMainScreenActivity.addDataToNavigationViewHeader: ERROR! No user is connected! This should not be possible!");
+        }
     }
 
 }
