017cf1faf923aeb82980251a07d71d35ee910388
Virgil Barcan <ghitza9404@gmail.com>
2015-12-24 00:49:19 +0200

    Start implementation of ContextMenu for ShoppingList:  - Added a proof of concept, not yet showing any result when Delete is pressed;

--- app/src/main/java/barcan/virgil/com/shopassistant/frontend/regular/UserShoppingListFragment.java
+++ app/src/main/java/barcan/virgil/com/shopassistant/frontend/regular/UserShoppingListFragment.java
@@ -1,9 +1,14 @@
 package barcan.virgil.com.shopassistant.frontend.regular;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -38,7 +43,59 @@ public class UserShoppingListFragment extends Fragment {
         //Get the shopping list from the Controller and populate the view
         populateShoppingList();
 
+        //Register listView for context menu
+        registerForContextMenu(listViewShoppingList);
+
         return view;
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, view, menuInfo);
+        MenuInflater contextMenuInflater = getActivity().getMenuInflater();
+        contextMenuInflater.inflate(R.menu.context_menu_shopping_list, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()) {
+            case R.id.delete_product:
+                System.out.println("Delete city: " + info.id);
+                //deleteCity(info.id);
+
+                showDeleteProduct().show();
+
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    /**
+     * The DeleteProduct AlertDialog
+     * @return the DeleteProduct AlertDialog
+     */
+    private AlertDialog showDeleteProduct() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+
+        alertDialogBuilder.setMessage(R.string.delete_product_from_shopping_list)
+                .setPositiveButton(R.string.delete_yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.out.println("YES");
+                    }
+                })
+                .setNegativeButton(R.string.delete_no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.out.println("NO");
+                    }
+                });
+
+        return alertDialogBuilder.create();
     }
 
     /**
